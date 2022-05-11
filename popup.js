410af59bef94448148aef241a53740986d40929e
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-05-11 17:53:29 -0500

    Creat getData function to call for array info

--- popup.js
+++ popup.js
@@ -9,28 +9,137 @@ open.addEventListener('click', () => {
   const popup = document.createElement('div');
   popup.className = 'popup'
   popup.innerHTML = `
-  <h3 id='subtitle' class='pad-bot'>Multi Post Stories</h3>
+  <h3 id='modal-title' class='pad-bot'>Multi Post Stories</h3>
   <button class='close'><i class="closeIcon material-icons ">close</i></button>
   <ul id="lang-boxes">
-  <li class="lang-box1 fs">html</li>
-  <li class="lang-box1 fs">Bootstrap</li>
-  <li class="lang-box1 fs">Ruby on rails</li>
+  <li id='technology1' class="lang-box1 fs"></li>
+  <li id='technology2' class="lang-box1 fs"></li>
+  <li id='technology3' class="lang-box1 fs"></li>
   </ul>
-  <img class='centerImage' src="./img/Snapshoot Portfolio.png" alt="">
-  <p class = 'font-size'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent</p>
+  <img id='featuredImage' class='centerImage' src="./img/Snapshoot Portfolio.png" alt="">
+  <p id='description' class = 'font-size'></p>
   <div class='center-btn'>
-    <button type='button' class='see-more btn-style' href='#'>See Live<img src="./img/icons/Icon - Export.svg" alt=""></button>
-    <button class='btn-style' type='button' href='#'>See Source<img src="./img/social/Icon -GitHub.svg" alt="Github"></button> 
+    <button id='link'><a class='btn-style' href='https://zeraltz.github.io/Portfolio-Website/'>See Live<img src="./img/icons/Icon - Export.svg" alt=""></a></button>
+    <button id='source'><a class='btn-style' href='https://github.com/Zeraltz/Portfolio-Website'>See Source<img src="./img/social/Icon -GitHub.svg" alt="Github"></a></button> 
   </div>
   `;
     
   main.appendChild(popup);
   body.appendChild(main);
+  
+  const getData = () => {
+    document.getElementById('modal-title').innerHTML = projectInformation[0].name
+    document.getElementById('description').innerHTML = projectInformation[0].description
+    document.getElementById('featuredImage').innerHTML = projectInformation[0].featuredImage
+    document.getElementById('technology1').innerHTML = projectInformation[0].technologies.technology1
+    document.getElementById('technology2').innerHTML = projectInformation[0].technologies.technology2
+    document.getElementById('technology3').innerHTML = projectInformation[0].technologies.technology3
+  }
+
+  getData();
 
   const close = document.querySelector('.close');
   close.addEventListener('click', () => {
     body.removeChild(main)
   })
 })
+
+let projectInformation = [
+{
+  name: 'Website Portfolio',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+{
+  name: 'Technology Company Website',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+{
+  name: 'Business Company App',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+{
+  name: 'Responsive Mobile App',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+{
+  name: 'Standalone App',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+{
+  name: 'Comparison App',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+{
+  name: 'Weather App',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  featuredImage: './img/Snapshoot Portfolio.png',
+  technologies: {
+    technology1: 'html',
+    technology2: 'bootstrap',
+    technology3: 'ruby'
+  },
+  link: 'https://zeraltz.github.io/Portfolio-Website/',
+  source: 'https://github.com/Zeraltz/Portfolio-Website'
+},
+];
+
+// const getData = () => {
+//   document.getElementById('modal-title').innerHTML = projectInformation[0].name
+//   document.getElementById('description').innerHTML = projectInformation[0].description
+//   document.getElementById('featuredImage').innerHTML = projectInformation[0].featuredImage
+//   document.getElementById('technology1').innerHTML = projectInformation[0].technologies.technology1
+//   document.getElementById('technology2').innerHTML = projectInformation[0].technologies.technology2
+//   document.getElementById('technology3').innerHTML = projectInformation[0].technologies.technology3
+//   document.getElementById('link').innerHTML = projectInformation[0].link
+//   document.getElementById('source').innerHTML = projectInformation[0].source
+// }
 
 
