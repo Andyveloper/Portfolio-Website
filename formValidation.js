c4eef1feb2e21e5d0ffecb88913a1f93f890ee7c
worashf <uppert83@gmail.com>
2022-05-13 00:46:48 +0300

    add email regex and form submit

--- formValidation.js
+++ formValidation.js
@@ -1,10 +1,20 @@
 const form = document.getElementById('large-form');
 
-const email = form.element('mail');
-let emailAdress = email.value;
+const email = document.getElementById("mail")
+const emailError= document.getElementById("error-message")
+const emailAdress = email.value;
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  if (emailAdress.match)
+  if (emailAdress.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)){
+    form.submit()
+  }
+  else{
+   emailError.textContent="Please enter the email only contain lowercase characters";
+   emailError.style.display="block"
+   emailError.style.color="red"
+   
+  }
+  return false;
 })
 
