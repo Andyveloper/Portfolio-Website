ef85ef126b7b0442a6c586fc1e2b1db8dc7753ed
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-05-12 17:41:22 -0500

    Finish form validation script

--- formValidation.js
+++ formValidation.js
@@ -1,20 +1,18 @@
 const form = document.getElementById('large-form');
-
 const email = document.getElementById("mail")
 const emailError= document.getElementById("error-message")
-const emailAdress = email.value;
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  if (emailAdress.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)){
-    form.submit()
-  }
-  else{
-   emailError.textContent="Please enter the email only contain lowercase characters";
+// ==============================================
+// EMAIL VALIDATION
+
+form.addEventListener('submit', (event) => {
+  const emailAdress = email.value;
+  const regEx = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+  if (!regEx.test(emailAdress)){
+   event.preventDefault();
+   emailError.innerHTML="Invalid email, lowercase required";
    emailError.style.display="block"
    emailError.style.color="red"
-   
   }
-  return false;
 })
 
