5300af7d6e26ac8f90f35394394339811eecfc55
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-05-12 17:43:56 -0500

    Fix eslints

--- formValidation.js
+++ formValidation.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('large-form');
-const email = document.getElementById("mail")
-const emailError= document.getElementById("error-message")
+const email = document.getElementById('mail');
+const emailError = document.getElementById('error-message');
 
 // ==============================================
 // EMAIL VALIDATION
@@ -8,11 +8,10 @@ const emailError= document.getElementById("error-message")
 form.addEventListener('submit', (event) => {
   const emailAdress = email.value;
   const regEx = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
-  if (!regEx.test(emailAdress)){
-   event.preventDefault();
-   emailError.innerHTML="Invalid email, lowercase required";
-   emailError.style.display="block"
-   emailError.style.color="red"
+  if (!regEx.test(emailAdress)) {
+    event.preventDefault();
+    emailError.innerHTML = 'Invalid email, lowercase required';
+    emailError.style.display = 'block';
+    emailError.style.color = 'red';
   }
-})
-
+});
