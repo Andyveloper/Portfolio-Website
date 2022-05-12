3cc74a0002014cc4ad8712b443a07c49c7462d8d
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-05-12 16:29:43 -0500

    First script

--- formValidation.js
+++ formValidation.js
@@ -1,5 +1,10 @@
 const form = document.getElementById('large-form');
 
+const email = form.element('mail');
+let emailAdress = email.value;
+
 form.addEventListener('submit', event => {
   event.preventDefault();
+  if (emailAdress.match)
 })
+
